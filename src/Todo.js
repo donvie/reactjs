30a614ec684e6ef84d9db2747d2ea5608198fb47
Administrator <heridonvi_tagaban12@yahoo.com>
2022-12-05 09:56:18 +0800

    Updated

---
+++ src/Todo.js
@@ -0,0 +1,84 @@
+import React  from 'react';
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        description: '',
+        todos: [
+            {
+                id: 1,
+                description: 'Coding',
+                isCompleted: false
+            },
+            {
+                id: 2,
+                description: 'Singing',
+                isCompleted: false
+            }
+        ]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({description: event.target.value});
+  }
+
+  addTodo () {
+    this.setState({
+        todos: [
+            ...this.state.todos,
+            ...[{
+                id: this.state.todos.length,
+                description: this.state.description,
+                isCompleted: false
+            }]
+        ]
+    })
+    this.setState({description: ''});
+  }
+
+  addTodo () {
+    this.setState({
+        todos: [
+            ...this.state.todos,
+            ...[{
+                id: this.state.todos.length,
+                description: this.state.description,
+                isCompleted: false
+            }]
+        ]
+    })
+    this.setState({description: ''});
+  }
+
+  deleteTodo (index) {
+    const todos = [...this.state.todos];
+    todos.splice(index, 1);
+    this.setState({todos});
+  }
+
+  render() {
+    return (
+      <div>
+        <input placeholder="Input your todo" value={this.state.description} type="text" onChange={this.handleChange} /> <br/>
+        <button onClick={this.addTodo}>Add Todo</button>
+        <h1>Todos {this.state.description}</h1>
+        {
+            this.state.todos.map((todo, index) => {
+                return <div key={index}>
+                    {todo.description}
+                    <button onClick={() => this.deleteTodo(index)}>X</button>
+                </div>
+            })
+        }
+      </div>
+    );
+  }
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from './App';
+import Todo from './Todo';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/todo",
+    element: <Todo />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
